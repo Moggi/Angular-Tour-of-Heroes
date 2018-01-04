a57a99361db79e29791590a6e7f8e7dd672c6ebb
Paulo Moggi <paulohpmoggi@gmail.com>
2018-01-04 15:59:04 -0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,17 +10,20 @@ import { MessagesComponent } from './messages/messages.component';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeroesComponent,
 		HeroDetailComponent,
-		MessagesComponent
+		MessagesComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
+		AppRoutingModule,
 	],
 	providers: [ HeroService, MessageService ],
 	bootstrap: [AppComponent]
