64f87e4e790a5309db0d5b49bffed4cc1b70880a
Paulo Moggi <paulohpmoggi@gmail.com>
2018-01-05 11:07:17 -0200

    CRUD Heroes - Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
 	declarations: [
@@ -24,6 +27,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 		BrowserModule,
 		FormsModule,
 		AppRoutingModule,
+		HttpClientModule,
+		
+		// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+		// and returns simulated server responses.
+		// Remove it when a real server is ready to receive requests.
+		HttpClientInMemoryWebApiModule.forRoot(
+			InMemoryDataService, { dataEncapsulation: false }
+		)
 	],
 	providers: [ HeroService, MessageService ],
 	bootstrap: [AppComponent]
